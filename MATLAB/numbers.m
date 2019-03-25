138871d602da424a867e49398f2cd5f2b890f874
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2019-03-25 07:48:24 +0300

    changing arguments in function calls

--- MATLAB/numbers.m
+++ MATLAB/numbers.m
@@ -1,13 +1,13 @@
 %this is the FL1L2 solver
 
 function [F_set, L1_set, L2_set, num] = numbers(x, y, xx, yy, tol)
-    F_sym = sym('f_%d_%d',3); % fumdamental matrix (unknown)
+    %F_sym = sym('f_%d_%d',3); % fumdamental matrix (unknown)
     syms L_1 L_2; % distortion parametrs (unknown)  
 
     %X = [F(1,1), F(1,2), F(2,1), F(2,2), L_1 * F(3,1), F(3,1), L_1*F(3,2),
     %    F(3,2), L_2*F(1,3), F(1,3), L_2*F(2,3), F(2,3), F(3,3), L_1*F(3,3),
     %    L_2*F(3,3), L_1*L_2*F(3,3)]
-    Q_sym = sym('q_%d_%d', [10 6]); % leftovers afret G-J elimination
+    %Q_sym = sym('q_%d_%d', [10 6]); % leftovers afret G-J elimination
 
     %[x, y, xx, yy, L1_th, L2_th, F_th] = generate_the_right_way();
 
@@ -21,24 +21,23 @@ function [F_set, L1_set, L2_set, num] = numbers(x, y, xx, yy, tol)
         end
         for k = 1 : (j - 1) %we don't use 4 first rows?
             temp = MG(k, j);
-            MG(k, j) = 0; %ndg;
+            MG(k, j) = 0; %may be dont do that
             for ind = 11 : 16
                 MG(k, ind) = MG(k, ind) - temp * MG(j, ind);
             end
         end
     end
-    %MG = rref(M);
     %disp(MG);
 
     Q = MG(:, 11:16);
     %disp(Q);
 
-    S = find_S(F_sym, L_1, L_2, Q_sym);
+    S = find_S(L_1, L_2, Q);
     [CX, ~, MG] = find_poly(S, L_1, L_2);
     %disp('I am back!');
     %disp(TX);
 
-    L1_all = roots(subs(CX, Q_sym, Q)); %comples roots
+    L1_all = roots(CX); %comples roots
 %     disp('THESE ARE L1');
 %     disp(L1_all);
     L1_new = zeros(10);
@@ -60,8 +59,8 @@ function [F_set, L1_set, L2_set, num] = numbers(x, y, xx, yy, tol)
     
     for i = 1:num
         L1 = L1_all(i);
-        L2 = find_L2_third_attempt(MG, Q_sym, Q, L_1, L1);
-        F = find_F(S, Q_sym, Q, L_1, L_2, L1, L2);
+        L2 = find_L2_third_attempt(MG, L_1, L1);
+        F = find_F(S, Q, L_1, L_2, L1, L2);
 %         disp('Here are the results for L1, L2:');
 %         disp([L1 L2]);
 %         disp('Here is the counted F:');
