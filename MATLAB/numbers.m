497395b86b40c09c337a7b7b133d25699e63a854
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2019-03-31 12:13:30 +0300

    changed finding Q form using QR to using mldivide

--- MATLAB/numbers.m
+++ MATLAB/numbers.m
@@ -1,6 +1,6 @@
 %this is the FL1L2 solver
 
-function [F_set, L1_set, L2_set, num, R] = numbers(x, y, xx, yy, tol)
+function [F_set, L1_set, L2_set, num] = numbers(x, y, xx, yy, tol)
     %F_sym = sym('f_%d_%d',3); % fumdamental matrix (unknown)
     %syms L_1 L_2; % distortion parametrs (unknown)  
 
@@ -12,25 +12,7 @@ function [F_set, L1_set, L2_set, num, R] = numbers(x, y, xx, yy, tol)
     %[x, y, xx, yy, L1_th, L2_th, F_th] = generate_the_right_way();
 
     M = find_M(x, y, xx, yy);
-    [~, MG] = qr(M);
-    for i = 1 : 10
-        j = 10 - i + 1;
-        temp = MG(j, j);
-        for k = j : 16
-            MG(j, k) = MG(j, k) / temp;
-        end
-        for k = 1 : (j - 1)
-            temp = MG(k, j);
-            MG(k, j) = 0; %do i need this?
-            for ind = 11 : 16
-                MG(k, ind) = MG(k, ind) - temp * MG(j, ind);
-            end
-        end
-    end
-    %MG = rref(M);
-    %disp(MG);
-
-    Q = MG(:, 11:16);
+    Q = M(:, 1:10)\M(:, 11:16);
     %disp(Q);
 
     [R12, R13, R14, R31, R32, R33, R34, R41, R42, R43, R44] = find_R_num(Q);
@@ -40,28 +22,28 @@ function [F_set, L1_set, L2_set, num, R] = numbers(x, y, xx, yy, tol)
 %     disp(L1_all);
     L1_new = zeros(10);
     num = 0;
-    for i = 1 : length(L1_all)
+    for i = 1 : 10
         if abs(imag(L1_all(i))) < tol
             num = num + 1;
             L1_new(num) = real(L1_all(i));
         end
     end
-    L1_new = L1_new(1:num);
-    L1_all = L1_new;
+    %L1_new = L1_new(1:num);
+    %L1_all = L1_new;
     
     F_set = zeros(num, 3, 3);
     L1_set = zeros(1, num);
     L2_set = zeros(1, num);
     
     for i = 1:num
-        L1 = L1_all(i);
+        L1 = L1_new(i);
         L2 = find_L2_fourth_attempt(L1, R12, R13, R14, R31, R32, R33, R34, R41, R42, R43, R44);
         F = find_F(Q, L1, L2);
 %         disp('Here are the results for L1, L2:');
 %         disp([L1 L2]);
 %         disp('Here is the counted F:');
 %         disp(F);
-        F_set(i, :, :) = F;
+        F_set(i, :, :) = F; %probably shoud change it to F(:,:,i) since that makes more sense
         L1_set(i) = L1;
         L2_set(i) = L2;
 
