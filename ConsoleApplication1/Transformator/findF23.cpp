fa32a761df14d5fae52bb069c777f8d9c26ada01
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2019-06-15 17:10:08 +0300

    Deleting C++ files for previous idea to count all in symbolic varibles and then transforming into code. That proved to be not working.

--- ConsoleApplication1/Transformator/findF23.cpp
+++
@@ -1,6 +0,0 @@
-#include "stdafx.h"
-#include <cmath>
-
-void findCoefficientsForF23(double ** F, double ** Q, double L1, double L2) {
-F[2][3]=-(Q[4][2]-L_1*Q[5][2]+L_1*(Q[4][3]-L_1*Q[5][3])+L_2*(Q[4][4]-L_1*Q[5][4])+L_1*L_2*(Q[4][5]-L_1*Q[5][5]))/(Q[4][1]-L_1*Q[5][1]+L_2*(Q[4][0]-L_1*Q[5][0]));
-}
